5e9cfb5fbc5d3b32ef2d19f8d4ee5614715453ff
Jarrod Norwell <official.antique@gmail.com>
2026-03-18 06:16:55 +0800

    Rewrote Guava for CombinedCores

--- Guava.swift
+++ Guava.swift
@@ -7,44 +7,102 @@
 
 import Foundation
 
-public enum NESKey : Int {
-    case a = 0,
-         b = 1
-    
-    case start = 3,
-         select = 2
-    
-    case up = 4,
-         down = 5,
-         left = 6,
-         right = 7
+struct GuavaCommon {
+    static let height: Int = 240
+    static let width: Int = 256
 }
 
-public class Guava {
-    public var emulator: Nes = .init()
+public actor Guava {
+    public let emulator: Nes = Nes()
+    
+    var audiobuffer: [Float32] = Array(repeating: 0, count: 735)
+    var videobuffer: [UInt8] = Array(repeating: 0, count: 256 * 240 * 4)
+    
+    var _paused: Bool = false,
+        _running: Bool = false
+        
     
     public init() {}
     
-    public func insert(_ cartridge: URL) throws {
-        abuffer.fill(with: 0)
-        fbuffer.fill(with: 0)
-        guard let data: NSData = .init(contentsOf: cartridge) else {
+    public func insert(cartridge: URL) throws {
+        audiobuffer.fill(with: 0)
+        videobuffer.fill(with: 0)
+        guard let data: NSData = NSData(contentsOf: cartridge) else {
             return
         }
         
-        var bytes: [Byte] = .init(repeating: 0, count: data.length)
+        var bytes: [Byte] = Array(repeating: 0, count: data.length)
         data.getBytes(&bytes, length: data.length)
         
         try emulator.loadRom(rom: bytes)
     }
     
+    
+    public func pause() {
+        paused = true
+    }
+    
+    public func start() {
+        running = true
+    }
+    
     public func step() {
+        if paused {
+            return
+        }
+        
         emulator.runFrame()
     }
     
     public func stop() {
+        pause()
         emulator.unloadRom()
+        unpause()
+        
+        running = false
     }
+    
+    public func unpause() {
+        paused = false
+    }
+    
+    
+    public var paused: Bool {
+        get {
+            _paused
+        }
+        set {
+            _paused = newValue
+        }
+    }
+    
+    public var running: Bool {
+        get {
+            _running
+        }
+        set {
+            _running = newValue
+        }
+    }
+    
+    
+    public func press(button: UInt32) {
+        emulator.setButtonPressed(pad: 0, button: Int(button))
+    }
+    
+    public func release(button: UInt32) {
+        emulator.setButtonReleased(pad: 0, button: Int(button))
+    }
+    
+    
+    public func load(state: URL) {
+        // emulator.load(state: state)
+    }
+    
+    public func save(state: URL) {
+        // emulator.save(state: state)
+    }
+    
     
     public var state: [UInt8] {
         get {
@@ -58,33 +116,18 @@ public class Guava {
         }
     }
     
-    public var paused: Bool = false
-    public var isPaused: Bool {
-        get {
-            paused
-        }
-        set {
-            paused = newValue
-        }
+    public func setState(for buffer: [UInt8]) throws {
+        try emulator.setState(state: buffer)
     }
     
-    var abuffer: [Float32] = .init(repeating: 0, count: 735)
-    public func audiobuffer(_ completion: @escaping (_ buffer: [Float32]) -> Void) {
-        emulator.setSamples(inside: &abuffer)
-        completion(abuffer)
+    
+    public func audioCallback(output: @escaping ([Float]) -> Void) {
+        emulator.setSamples(inside: &audiobuffer)
+        output(audiobuffer)
     }
     
-    var fbuffer: [UInt8] = .init(repeating: 0, count: 256 * 240 * 4)
-    public func framebuffer(_ completion: @escaping (_ buffer: [UInt8], _ width: Int, _ height: Int) -> Void) {
-        emulator.setPixels(inside: &fbuffer)
-        completion(fbuffer, 256, 240)
-    }
-    
-    public func button(button: NESKey, player: Int, pressed: Bool) {
-        if pressed {
-            emulator.setButtonPressed(pad: player, button: button.rawValue)
-        } else {
-            emulator.setButtonReleased(pad: player, button: button.rawValue)
-        }
+    public func videoCallback(output: @escaping ([UInt8], Int, Int) -> Void) {
+        emulator.setPixels(inside: &videobuffer)
+        output(videobuffer, GuavaCommon.width, GuavaCommon.height)
     }
 }
